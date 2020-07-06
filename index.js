423c30d6d92c943269e182b4bac3d7b278a9ef0a
Matthew M <mbmoorin@gmail.com>
2020-07-06 16:15:56 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -7,7 +7,7 @@ function nestedTarget() {
 }
 
 function increaseRankBy(n) {
-  var lis = document.querySelector('.ranked-list');
+  var lis = document.querySelectorAll('.ranked-list');
   
   for (let i = 0; i < lis.length; i++) {
     lis[i].innerHTML = parseInt((lis[i].innerHTML)+n);
